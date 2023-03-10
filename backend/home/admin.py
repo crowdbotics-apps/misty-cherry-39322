0f629827f519322aa57279d85d0af082852bc958
crowdbotics-apps <team@crowdbotics.com>
2023-03-10 20:54:50 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import LanguagePhrase
+admin.site.register(LanguagePhrase)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class LanguagePhrase(models.Model):
+    'Generated Model'
+    language = models.TextField()
+    phrase = models.TextField()
